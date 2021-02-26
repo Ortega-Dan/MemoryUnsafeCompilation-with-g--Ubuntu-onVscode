b686e713988174e080db5646b7808fb2dff4eb19
Dan Ortega <ortega.dan2010@gmail.com>
2021-02-26 18:19:57 -0500

    Adding unsafe memory compilation

---
+++ MemoryInjection.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  char *p1 = "Rosebud"; // the top secret password
+  char *p2 = "Safe";
+  long buffer[1];
+
+  cout << &p1 << endl;
+  cout << &p2 << endl;
+  cout << buffer << endl;
+
+  cin >> buffer[1]; // actually referencing the pointer 'p1'
+  cout << p1;
+
+  return 0;
+}
